0e80dfae5cb3e75229afb423748606b6253eac8f
josewilmerDR <josewdr@gmail.com>
2023-04-06 04:59:41 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  setInterval(generateCard, 10000);
+  generateCard();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const updateButton = document.querySelector(
+    "button[onclick='updateCardSize();']"
+  );
+  const generateButton = document.getElementById("generate-card");
+
+  updateButton.addEventListener("click", updateCardSize);
+  generateButton.addEventListener("click", generateCard);
 };
+
+function generateCard() {
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const suits = ["♥", "♠", "♣", "♦"];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  document.getElementById("value").innerHTML = randomValue;
+  document.getElementById("top-left").innerHTML = randomSuit;
+  document.getElementById("top-left").setAttribute("data-suit", randomSuit);
+  document.getElementById("bottom-right").innerHTML = randomSuit;
+  document.getElementById("bottom-right").setAttribute("data-suit", randomSuit);
+}
+
+function updateCardSize() {
+  const widthInput = document.getElementById("width-input");
+  const heightInput = document.getElementById("height-input");
+  const card = document.getElementById("card");
+
+  const width = parseInt(widthInput.value);
+  const height = parseInt(heightInput.value);
+
+  card.style.width = width + "px";
+  card.style.height = height + "px";
+}
